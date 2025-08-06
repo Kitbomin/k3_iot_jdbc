896e5fdfc2a9a0fd36cc2b85204cd825e7667108
Kitbomin <zzxxcc1433@naver.com>
2025-08-06 12:49:38 +0900

    DB 연동 + CRUD 기능 구현 중 Create 구현

--- src/main/java/org/example/이론/First.java
+++ src/main/java/org/example/이론/First.java
@@ -50,26 +50,70 @@ public class First {
 
 
             // 2) DB 데이터 조회 (READ - 전체 조회)
-            String sql = "SELECT * FROM members";
-            stmt = conn.createStatement();
-            rs = stmt.executeQuery(sql); // SELECT 실행 - 반환은 ResultSet 객체
+//            String sql = "SELECT * FROM members";
+//            stmt = conn.createStatement();
+//            rs = stmt.executeQuery(sql); // SELECT 실행 - 반환은 ResultSet 객체
+//
+//            while (rs.next()) { // rs.next(): 다음 행 존재여부 확인 boolean으로 반환함
+//                int id = rs.getInt("id");
+//                String name = rs.getString("name");
+//                String email = rs.getString("email");
+//
+//                System.out.println(id + " + " + name + " + " + email );
+//            }
 
-            while (rs.next()) { // rs.next(): 다음 행 존재여부 확인 boolean으로 반환함
-                int id = rs.getInt("id");
-                String name = rs.getString("name");
-                String email = rs.getString("email");
+            // 3) DB 데이터 조회 (READ - 단건 조회)
+//            String sql = "SELECT * FROM members WHERE id = ?";
+//            pstmt = conn.prepareStatement(sql);
+//            pstmt.setInt(1,2); // parameter 동적 파라미터의 위치 값 -> 1부터 시작함
+//
+//            rs = pstmt.executeQuery(sql);
+//
+//            if (rs.next()) {
+//                int id = rs.getInt("id");
+//                String name = rs.getString("name");
+//                String email = rs.getString("email");
+//
+//                System.out.println(id + " + " + name + " + " + email );
+//            } else { //rs.next 값이 없다면 ?
+//                System.out.println("해당 ID의 회원이 없습니당");
+//            }
 
-                System.out.println(id + " + " + name + " + " + email );
-            }
 
+            // 4) DB 데이터 수정 (Update)
+//            String sql = "UPDATE members SET email = ? WHERE id = ?";
+//            pstmt = conn.prepareStatement(sql);
+//            pstmt.setString(1, "newEmail.example.com");
+//            pstmt.setInt(2,1);
+//
+//            int updatedResult = pstmt.executeUpdate();
+//            System.out.println(updatedResult + " 명 수정됨 ");
 
-
-
+            // 5) DB 데이터 삭제 (DELETE)
+//            String sql = "DELETE FROM members WHERE id = ?";
+//            pstmt = conn.prepareStatement(sql);
+//            pstmt.setInt(1,1);
+//            int deletedResult = pstmt.executeUpdate();
+//            System.out.println(deletedResult + " 명 삭제됨 ");
 
 
         }catch (Exception e) {
             System.out.println("DB와의 커넥션 중 오류 발생");
             e.printStackTrace(); // 예이 발생 시 예외메시지 + 호출 스택정보 출력(예외 발생지를 출력해줌) -> 개발과 디버깅 과정 중에서 유용함
+        }
+        finally {
+            // 자원 해제 (자바 7버전 이상 -> 자원이 자동 해제가 됨) => 생략이 가능!
+            try {
+                if (conn != null && stmt != null && rs != null) {
+                    // DB connection이 모두 존재한다면
+                    stmt.close();
+                    rs.close();
+                    conn.close();
+                    System.out.println("DB 연결 종료");
+                }
+            }catch (Exception e) {
+                e.printStackTrace();
+            }
         }
 
 
